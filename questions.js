3c3bfdb0b38f61bc5116eabc5e42bb5049524859
burmesetools <burmesetools@gmail.com>
2026-07-19 00:59:17 +0630

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,8 +1,19 @@
-
 const questions = [
-  {
-    question: "Which country is famous for the Eiffel Tower?",
-    options: ["Myanmar", "France", "Japan", "Brazil"],
-    answer: 1
-  }
+{
+ question:"Which country is famous for the Eiffel Tower?",
+ options:["Myanmar","France","Japan","Brazil"],
+ answer:1
+},
+
+{
+ question:"Who is the creator of Windows?",
+ options:["Bill Gates","Elon Musk","Steve Jobs","Mark Zuckerberg"],
+ answer:0
+},
+
+{
+ question:"What planet do we live on?",
+ options:["Mars","Earth","Venus","Jupiter"],
+ answer:1
+}
 ];
