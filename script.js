6464906de64a538409f262314e8d3d41ed8640e0
burmesetools <burmesetools@gmail.com>
2026-07-19 00:29:39 +0630

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-// QuizVerse JavaScript
 function startQuiz() {
-    alert("Quiz Coming Soon 🚀");
+  document.querySelector(".card").innerHTML = `
+    <h2>Question 1 / 1</h2>
+    <p>Which country is famous for the Eiffel Tower?</p>
+
+    <button onclick="checkAnswer(0)">Myanmar</button><br><br>
+    <button onclick="checkAnswer(1)">France</button><br><br>
+    <button onclick="checkAnswer(2)">Japan</button><br><br>
+    <button onclick="checkAnswer(3)">Brazil</button>
+  `;
+}
+
+function checkAnswer(answer) {
+  if (answer === 1) {
+    alert("✅ Correct!");
+  } else {
+    alert("❌ Wrong!");
+  }
 }
